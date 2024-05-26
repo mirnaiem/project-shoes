585af0d582a7c924e02512df236216f734a78b3a
mirnaiem <mirnaiem8teen@gmail.com>
2024-05-26 15:24:44 +0600

    google login done

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -1,8 +1,13 @@
+import useAuth from "../hooks/useAuth";
 
 const GoogleLogin = () => {
+ const {googleLogin}=useAuth()
+ const handleGoogleLogin=()=>{
+  googleLogin()
+ }
  return (
   <div>
-   <button className="py-3 px-3 text-white  bg-blue-500 w-full mt-2 rounded-md">Login with Google</button>
+   <button onClick={handleGoogleLogin} className="py-3 px-3 text-white  bg-blue-500 w-full mt-2 rounded-md">Login with Google</button>
   </div>
  );
 };
